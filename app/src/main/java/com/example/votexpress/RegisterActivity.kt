0ee6b2d76466c18a0f47ca76a1cb949064b3aa19
Joshua Peralta <peralta.joshuapaul07@gmail.com>
2024-10-17 17:46:42 +1300

    update register activity and add new contact us activity

--- app/src/main/java/com/example/votexpress/RegisterActivity.kt
+++ app/src/main/java/com/example/votexpress/RegisterActivity.kt
@@ -2,13 +2,24 @@ package com.example.votexpress
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var fullNameEditText: EditText
+    private lateinit var emailEditText: EditText
+    private lateinit var voterIdEditText: EditText
+    private lateinit var passwordEditText: EditText
+    private lateinit var confirmPasswordEditText: EditText
+    private lateinit var registerBtn: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -19,6 +30,13 @@ class RegisterActivity : AppCompatActivity() {
             insets
         }
 
+        fullNameEditText = findViewById(R.id.etFullName)
+        emailEditText = findViewById(R.id.etEmail)
+        voterIdEditText = findViewById(R.id.etVoterId)
+        passwordEditText= findViewById(R.id.etPassword)
+        confirmPasswordEditText = findViewById(R.id.etConfirmPassword)
+        registerBtn = findViewById(R.id.btnRegister)
+
         val tvHaveAccount = findViewById<TextView>(R.id.tvAlreadyHaveAccount)
 
         tvHaveAccount.setOnClickListener {
@@ -26,5 +44,44 @@ class RegisterActivity : AppCompatActivity() {
             startActivity(intent)
             finish()
         }
+
+        registerBtn.setOnClickListener {
+            val fullName = fullNameEditText.text.toString().trim()
+            val email = emailEditText.text.toString().trim()
+            val voterId = voterIdEditText.text.toString().trim()
+            val password = passwordEditText.text.toString().trim()
+            val confirmPassword = confirmPasswordEditText.text.toString().trim()
+
+            if(validateInput(fullName, email, voterId, password, confirmPassword)){
+                Toast.makeText(this, "Registration successful!",Toast.LENGTH_SHORT).show()
+
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+
+
+        }
+    }
+
+    private fun validateInput(fullName: String, email: String, voterId: String, password: String, confirmPassword: String): Boolean {
+
+        if (fullName.isEmpty() || email.isEmpty() || voterId.isEmpty() || password.isEmpty() || confirmPassword.isEmpty()) {
+            Toast.makeText(this, "Please fill all fields", Toast.LENGTH_SHORT).show()
+            return false
+        }
+        if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            Toast.makeText(this, "Please enter a valid email", Toast.LENGTH_SHORT).show()
+            return false
+        }
+        if (password != confirmPassword) {
+            Toast.makeText(this, "Passwords do not match", Toast.LENGTH_SHORT).show()
+            return false
+        }
+        if (password.length < 6) {
+            Toast.makeText(this, "Password must be at least 6 characters long", Toast.LENGTH_SHORT).show()
+            return false
+        }
+        return true
     }
 }
